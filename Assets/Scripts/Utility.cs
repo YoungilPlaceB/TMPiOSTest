918bc4a00db355027e5661b690effcec9d978819
youngil <youngil@placeb.com>
2024-04-22 18:52:26 +0900

    Keyboard Height 함수 추가

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,32 @@
+public class Utility
+{
+    public static int GetKeyboardHeight(bool includeInput = true)
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        return 0;
+#elif UNITY_ANDROID
+        using (AndroidJavaClass unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            AndroidJavaObject unityPlayer = unityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
+            AndroidJavaObject view = unityPlayer.Call<AndroidJavaObject>("getView");
+            AndroidJavaObject dialog = unityPlayer.Get<AndroidJavaObject>("mSoftInputDialog");
+            if (view == null || dialog == null)
+                return 0;
+            var decorHeight = 0;
+            if (includeInput)
+            {
+                AndroidJavaObject decorView = dialog.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView");
+                if (decorView != null)
+                    decorHeight = decorView.Call<int>("getHeight");
+            }
+            using (AndroidJavaObject rect = new AndroidJavaObject("android.graphics.Rect"))
+            {
+                view.Call("getWindowVisibleDisplayFrame", rect);
+                return Screen.height - rect.Call<int>("height") + decorHeight;
+            }
+        }
+#elif UNITY_IOS
+        return (int)TouchScreenKeyboard.area.height;
+#endif
+    }
+}
